6fafcdbfb7f9793f87adcf201b0624917d059cd9
MTayyab4 <126855912+MTayyab4@users.noreply.github.com>
2024-05-02 20:54:19 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,20 @@
-//sw.js
-const staticAssets = [
-  "/", 
-  "/index.html",
-  "/404.html",
-  "/offline.html",
-];
+const staticAssets = ["/", "/index.html", "/404.html", "/offline.html"];
 let cacheVersion = 0;
-let cacheName = `cache-v${cacheVersion}`;
+let cacheName = cache-v${cacheVersion};
+
 function increment() {
-  cacheVersion ++;
-  cacheName = `cache-v${cacheVersion}`;
+  cacheVersion++;
+  cacheName = cache-v${cacheVersion};
 }
 
-//Add cache while installing Sw
+// Add cache while installing Sw
 self.addEventListener("install", (event) => {
   console.log("Attempting to install service worker and cache static assets");
   event.waitUntil(
     caches
       .open(cacheName)
       .then((cache) => {
-         //Update version
-         increment();
-
-        //add files to the cache
+        increment(); // Update version
         return cache.addAll(staticAssets);
       })
       .catch((err) => console.log(err))
@@ -31,7 +23,6 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   console.log("Activating new service worker...");
-
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
@@ -47,35 +38,37 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   console.log("Fetch event for", event.request.url);
-
   event.respondWith(
     caches
       .match(event.request)
       .then((response) => {
-        //If the response is found in the cache
         if (response) {
-          console.log("Found ", event.request.url, " in cache");
+          console.log("Found", event.request.url, "in cache");
           return response;
         }
-
-        return fetch(event.request).then((response) => {
-          // If a response is not found
-          if (response.status === 404) {
+        return fetch(event.request)
+          .then((response) => {
+            if (response.status === 404) {
+              return caches.open(cacheName).then((cache) => {
+                return cache.match("404.html");
+              });
+            }
+            if (response.ok) {
+              return caches.open(cacheName).then((cache) => {
+                cache.put(event.request.url, response.clone());
+                return response;
+              });
+            }
+          })
+          .catch((error) => {
+            console.log("Error,", error);
             return caches.open(cacheName).then((cache) => {
-              return cache.match("404.html");
+              return cache.match("offline.html");
             });
-          }
-
-          //Caching and returning the response if it doesn't exist in the cache
-          return caches.open(cacheName).then((cache) => {
-            cache.put(event.request.url, response.clone());
-            return response;
           });
-        });
       })
-      .catch(async (error) => {
-        console.log("Error, ", error);
-        //If page is offline/ Network failure
+      .catch((error) => {
+        console.log("Error,", error);
         return caches.open(cacheName).then((cache) => {
           return cache.match("offline.html");
         });
